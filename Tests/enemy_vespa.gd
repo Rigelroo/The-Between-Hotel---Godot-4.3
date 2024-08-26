ed102bc9fa1558d8b08915766be53ed8d97fb099
Rigelroo <felipemeirelis04@gmail.com>
2024-08-26 10:35:27 -0300

    Update wasp 1
    update wasp part 1

---
+++ Tests/enemy_vespa.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+var speed = 300
+
+var velox = Vector2.ZERO
+#
+#enum {
+	#SURROUND,
+	#ATTACK,
+	#HIT,
+#}
+#
+#var state = SURROUND
+#
+#func _physics_process(delta: float) -> void:
+	#match state:
+		#SURROUND:
+			#move()
+			#
+#
+#func move(target, delta):
+	#var direction = (target - global_position).normalized()
+	
